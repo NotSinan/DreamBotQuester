6194ea0dcbbdfed90dd268b55e632aa5b36dfb90
Sinan <haxsmith77@gmail.com>
2022-12-24 00:52:37 +1100

    Added Rune Mysteries quest.

---
+++ src/main/java/org/dreambot/quests/runemysteries/RuneMysteries.java
@@ -0,0 +1,10 @@
+package org.dreambot.quests.runemysteries;
+
+import org.dreambot.framework.Branch;
+
+public class RuneMysteries extends Branch {
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+}
