ce360f8e461e2fb9a5ba80479be027fa15f10139
Sinan <haxsmith77@gmail.com>
2022-12-25 02:05:37 +1100

    Added Romeo and Juliet quest.

---
+++ src/main/java/org/dreambot/quests/romeoandjuliet/TalkToJulietLeaf.java
@@ -0,0 +1,40 @@
+package org.dreambot.quests.romeoandjuliet;
+
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.dialogues.Dialogues;
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.settings.PlayerSettings;
+import org.dreambot.api.utilities.Logger;
+import org.dreambot.framework.Leaf;
+import org.dreambot.utilities.QuestHelper;
+import org.dreambot.utilities.QuestVarPlayer;
+import org.dreambot.utilities.Timing;
+
+public class TalkToJulietLeaf extends Leaf {
+
+    private final Area JULIET_AREA = new Area(3155, 3426, 3161, 3425, 1);
+    private final String[] DIALOGUE_OPTIONS = {"Ok, thanks."};
+
+
+    @Override
+    public boolean isValid() {
+        return PlayerSettings.getConfig(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 10 ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 50 && Inventory.contains("Cadava potion") ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 50 && PlayerSettings.getBitValue(12139) == 1;
+    }
+
+    @Override
+    public int onLoop() {
+        if (PlayerSettings.getBitValue(12139) == 0) {
+            QuestHelper.goAndTalkToNpc(JULIET_AREA, "Juliet", DIALOGUE_OPTIONS);
+            return Timing.loopReturn();
+        } else {
+            if (Dialogues.inDialogue()) {
+                if (Dialogues.canContinue()) {
+                    Dialogues.continueDialogue();
+                }
+            }
+        }
+        return Timing.loopReturn();
+    }
+}
--- src/main/java/org/dreambot/quests/romeoandjuliet/TalkToRomeoLeaf.java
+++ src/main/java/org/dreambot/quests/romeoandjuliet/TalkToRomeoLeaf.java
@@ -10,47 +10,35 @@ import org.dreambot.api.methods.walking.impl.Walking;
 import org.dreambot.api.utilities.Sleep;
 import org.dreambot.api.wrappers.interactive.NPC;
 import org.dreambot.framework.Leaf;
+import org.dreambot.utilities.QuestHelper;
 import org.dreambot.utilities.QuestVarPlayer;
 import org.dreambot.utilities.Timing;
 
 public class TalkToRomeoLeaf extends Leaf {
 
-    private final Area VARROCK_SHOP_AREA = new Area(3209, 3421, 3223, 3409);
     private final Area ROMEO_AREA = new Area(3205, 3438, 3223, 3422);
+    private final String[] DIALOGUE_OPTIONS = {"Yes, I have seen her actually!", "Yes, ok, I'll let her know.", "Yes."};
 
     @Override
     public boolean isValid() {
-        return PlayerSettings.getConfig(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 0;
+        return PlayerSettings.getConfig(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 0 ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 20 ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_ROMEO_AND_JULIET.getId()) == 60;
     }
+
 
     @Override
     public int onLoop() {
-        if (!ROMEO_AREA.contains(Players.getLocal())) {
-            if (Walking.shouldWalk(4)) {
-                Walking.walk(ROMEO_AREA.getRandomTile());
+        if (PlayerSettings.getBitValue(12139) == 0) {
+            QuestHelper.goAndTalkToNpc(ROMEO_AREA, "Romeo", DIALOGUE_OPTIONS);
+            return Timing.loopReturn();
+        } else {
+            if (Dialogues.inDialogue()) {
+                if (Dialogues.canContinue()) {
+                    Dialogues.continueDialogue();
+                }
             }
-            return Timing.loopReturn();
         }
-
-        if (!Dialogues.inDialogue()) {
-            NPC romeo = NPCs.closest(npc -> npc.getName().equals("Romeo"));
-            if (romeo != null && romeo.interact("Talk-to")) {
-                Sleep.sleepUntil(() -> Dialogues.inDialogue(), 3000);
-            }
-            return Timing.loopReturn();
-        }
-
-        if (Dialogues.canContinue()) {
-            Dialogues.continueDialogue();
-            Sleep.sleepUntil(() -> !Dialogues.isProcessing(), 3000);
-            return Timing.loopReturn();
-        }
-
-//        if (Dialogues.areOptionsAvailable()) {
-//            Dialogues.chooseFirstOption("")
-//        }
-
         return Timing.loopReturn();
-
     }
 }
