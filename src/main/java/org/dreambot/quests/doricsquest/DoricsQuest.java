e57247b3059398b6ac8f836e1dd641f409ed1905
Sinan <haxsmith77@gmail.com>
2022-12-26 01:17:54 +1100

    Adding support for Dorics Quest

---
+++ src/main/java/org/dreambot/quests/doricsquest/DoricsQuest.java
@@ -0,0 +1,10 @@
+package org.dreambot.quests.doricsquest;
+
+import org.dreambot.framework.Branch;
+
+public class DoricsQuest extends Branch {
+    @Override
+    public boolean isValid() {
+        return true;
+    }
+}
