32bf1820988faa5607be13cab468bc91562072ce
Sinan <haxsmith77@gmail.com>
2022-12-27 17:57:25 +1100

    Added TalkToTrufitusLeaf.java

---
+++ src/main/java/org/dreambot/quests/junglepotion/TalkToTrufitusLeaf.java
@@ -0,0 +1,37 @@
+package org.dreambot.quests.junglepotion;
+
+import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.map.Area;
+import org.dreambot.api.methods.settings.PlayerSettings;
+import org.dreambot.framework.Leaf;
+import org.dreambot.utilities.QuestHelper;
+import org.dreambot.utilities.QuestVarPlayer;
+
+public class TalkToTrufitusLeaf extends Leaf {
+
+    private final Area TRUFITUS_AREA = new Area(2805, 3089, 2814, 3082);
+    private final String[] DIALOGUE_OPTIONS = {
+            "It's a nice village, where is everyone?",
+            "Me? How can I help?",
+            "It sounds like just the challenge for me.",
+            "Yes.",
+            "Of course!"
+    };
+
+    private final String TRUFITUS = "Trufitis";
+
+    @Override
+    public boolean isValid() {
+        return PlayerSettings.getConfig(QuestVarPlayer.QUEST_JUNGLE_POTION.getId()) == 0 ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_JUNGLE_POTION.getId()) == 1 && Inventory.contains("Snake weed") ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_JUNGLE_POTION.getId()) == 4 && Inventory.contains("Ardrigal") ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_JUNGLE_POTION.getId()) == 6 && Inventory.contains("Sito foil") ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_JUNGLE_POTION.getId()) == 8 && Inventory.contains("Volencia moss") ||
+                PlayerSettings.getConfig(QuestVarPlayer.QUEST_JUNGLE_POTION.getId()) == 10 && Inventory.contains("Rogue's purse");
+    }
+
+    @Override
+    public int onLoop() {
+        return QuestHelper.goAndTalkToNpc(TRUFITUS_AREA, TRUFITUS, DIALOGUE_OPTIONS);
+    }
+}
