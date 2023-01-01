1521457fe116466cfa6d0a02c4d1f70eba865de3
Sinan <haxsmith77@gmail.com>
2023-01-01 19:30:03 +1100

    Updated the retrieval of beads in Imp Catcher.

--- src/main/java/org/dreambot/quests/freequests/impcatcher/RetrieveBeadsLeaf.java
+++ src/main/java/org/dreambot/quests/freequests/impcatcher/RetrieveBeadsLeaf.java
@@ -1,21 +1,24 @@
 package org.dreambot.quests.freequests.impcatcher;
 
 import org.dreambot.api.methods.container.impl.Inventory;
+import org.dreambot.api.methods.dialogues.Dialogues;
 import org.dreambot.api.methods.interactive.NPCs;
 import org.dreambot.api.methods.interactive.Players;
 import org.dreambot.api.methods.item.GroundItems;
 import org.dreambot.api.methods.map.Area;
 import org.dreambot.api.methods.walking.impl.Walking;
+import org.dreambot.api.utilities.Logger;
 import org.dreambot.api.utilities.Sleep;
 import org.dreambot.api.wrappers.interactive.NPC;
 import org.dreambot.api.wrappers.items.GroundItem;
 import org.dreambot.framework.Leaf;
 import org.dreambot.utilities.Interaction;
+import org.dreambot.utilities.QuestHelper;
 import org.dreambot.utilities.Timing;
 
 public class RetrieveBeadsLeaf extends Leaf {
 
-    private final Area IMP_SPAWN_AREA = new Area(2623, 3218, 2637, 3200);
+    private final Area IMP_SPAWN_AREA = new Area(2816, 3188, 2840, 3166);
 
     @Override
     public boolean isValid() {
@@ -25,6 +28,10 @@ public class RetrieveBeadsLeaf extends Leaf {
     @Override
     public int onLoop() {
 
+        if (Inventory.count("Coins") < 500) {
+            QuestHelper.withdrawFromBank("Coins", 1000);
+        }
+
         if (!IMP_SPAWN_AREA.contains(Players.getLocal())) {
             if (Walking.shouldWalk(4)) {
                 Interaction.delayWalk(IMP_SPAWN_AREA.getRandomTile());
@@ -32,26 +39,33 @@ public class RetrieveBeadsLeaf extends Leaf {
             return Timing.loopReturn();
         }
 
-        GroundItem beads = GroundItems.closest(item -> item.getName().endsWith("beads"));
-        if (beads != null) {
-            if(Interaction.delayEntityInteract(beads, "Take")) {
-                Sleep.sleepUntil(() -> !beads.exists(), () -> Players.getLocal().isMoving(), 3000, 100);
+        GroundItem bead = GroundItems.closest(item -> item.getName().endsWith("bead") && IMP_SPAWN_AREA.contains(item));
+        if (bead != null) {
+            if (Interaction.delayEntityInteract(bead, "Take")) {
+                Logger.log("Bead on floor.");
+                Sleep.sleep(2000, 3000);
+            }
+            return Timing.loopReturn();
+        }
+
+        if (Dialogues.inDialogue()) {
+            if (Dialogues.canContinue()) {
+                Dialogues.continueDialogue();
             }
             return Timing.loopReturn();
         }
 
         if (!Players.getLocal().isInCombat()) {
-            NPC imp = NPCs.closest("Imp");
-            if (imp != null && Interaction.delayEntityInteract(imp, "Attack")) {
+            NPC imp = NPCs.closest(i -> i.getName().equals("Imp") && IMP_SPAWN_AREA.contains(i) && i.isOnScreen());
+            if (imp != null && imp.canReach() && Interaction.delayEntityInteract(imp, "Attack")) {
                 Sleep.sleepUntil(() -> Players.getLocal().isInCombat(), 3000);
             }
             return Timing.loopReturn();
         }
 
-        if (!Walking.isRunEnabled()) {
-            Timing.sleepForDelay();
-            if(Walking.toggleRun()) {
-                Sleep.sleepUntil(Walking::isRunEnabled, 3000);
+        if (!Walking.isRunEnabled() && Walking.getRunEnergy() > 20) {
+            if (Walking.toggleRun()) {
+                Sleep.sleepUntil(() -> Walking.isRunEnabled(), 3000);
             }
         }
 
