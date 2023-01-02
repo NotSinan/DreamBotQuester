269f165084274a88374bf9364d36c7c7df4ba967
Sinan <haxsmith77@gmail.com>
2023-01-02 18:18:32 +1100

    Updated quest logic.

--- src/main/java/org/dreambot/quests/freequests/vampyreslayer/RetrieveCoinsLeaf.java
+++ src/main/java/org/dreambot/quests/freequests/vampyreslayer/RetrieveCoinsLeaf.java
@@ -1,19 +1,17 @@
 package org.dreambot.quests.freequests.vampyreslayer;
 
 import org.dreambot.api.methods.container.impl.Inventory;
-import org.dreambot.api.methods.settings.PlayerSettings;
 import org.dreambot.framework.Leaf;
 import org.dreambot.utilities.QuestHelper;
-import org.dreambot.utilities.QuestVarPlayer;
 
 public class RetrieveCoinsLeaf extends Leaf {
     @Override
     public boolean isValid() {
-        return PlayerSettings.getConfig(QuestVarPlayer.QUEST_VAMPYRE_SLAYER.getId()) == 1 &&
-                Inventory.count("Coins") < 100 &&
-                Inventory.contains("Garlic");
+        return Inventory.count("Coins") < 50;
     }
 
     @Override
-    public int onLoop() { return QuestHelper.withdrawFromBank("Coins", 100); }
+    public int onLoop() {
+        return QuestHelper.withdrawFromBank("Coins", 100);
+    }
 }
