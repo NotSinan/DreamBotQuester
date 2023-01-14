158b7b179ab86eaf98c5451aa22934919609ce18
Sinan <haxsmith77@gmail.com>
2023-01-14 22:23:12 +1100

    update.

--- src/main/java/org/dreambot/quests/freequests/xmarksthespot/ClueStepFourLeaf.java
+++ src/main/java/org/dreambot/quests/freequests/xmarksthespot/ClueStepFourLeaf.java
@@ -31,7 +31,6 @@ public class ClueStepFourLeaf extends Leaf {
             if (Sleep.sleepUntil(() -> Players.getLocal().isAnimating(), 3000)) {
                 Sleep.sleepUntil(() -> !Players.getLocal().isAnimating(), 3000);
 
-
             }
         }
         return Timing.loopReturn();
